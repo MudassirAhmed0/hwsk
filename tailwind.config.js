b306cabdbd15e099279e29ae6c0928cda9c59714
Itz-zafeer <zafeerqz@gmail.com>
2023-01-15 01:34:54 +0500

    43434

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        red: "#ff0016",
+        black:"#282828",
+        gray:"#5c5c5c",
+      },
+      
+    },
+  },
+  plugins: [],
+}
