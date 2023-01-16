fbdf26e477238544e22258636a82d508bbcbe653
Itz-zafeer <zafeerqz@gmail.com>
2023-01-16 05:16:35 +0500

    5435345

--- assets/javascript/supportinghome.js
+++ assets/javascript/supportinghome.js
@@ -1,7 +1,10 @@
-var swiper = new Swiper(".supporting", {
+const supportingGap = window.innerWidth >768 ?  window.innerWidth / 100 * 3.7 :30
+var supportingSwiper = new Swiper(".supporting", {
     slidesPerView: "auto",
-    spaceBetween: 30,
+    spaceBetween: supportingGap ,
     loop: true,
+    autoplay:true,
+    grabCursor: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: false,
